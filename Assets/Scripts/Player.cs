0674a38fac162a63f20c1f0b81e6b31fb6a94681
TyLanger <ty.langer16@gmail.com>
2022-07-15 14:52:02 -0600

    Movement and Floor Plane
    Simple commit to check if I didn't break github

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float moveSpeed = 10;
+    Vector3 moveInput;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        //transform.position = Vector3.MoveTowards(transform.position, transform.position + moveInput, moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + (moveInput.normalized * moveSpeed * Time.fixedDeltaTime));
     }
 }
